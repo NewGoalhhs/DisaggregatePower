4665b167c6f62736dd1e4c7e1f4a397e75fef7ce
NandoJans <90688887+NandoJans@users.noreply.github.com>
2024-05-15 13:04:50 +0200

    Added loading bar to migration and for future implementation

--- core/Migration.py
+++ core/Migration.py
@@ -1,13 +1,36 @@
 import logging
 
+from helper.LoadingBarHelper import LoadingBarHelper
+
+
 class Migration:
     def __init__(self, db):
         self.db = db
         self.queries = []
+        self.lb = None
 
         # Set up logging
         logging.basicConfig(filename='migration.log', level=logging.INFO)
         self.logger = logging.getLogger()
+
+    def add_loading_bar(self, lb):
+        self.lb = lb
+
+    def update_loading_bar(self, current):
+        if self.lb is not None:
+            self.lb.update(current)
+
+    def finish_loading_bar(self):
+        if self.lb is not None:
+            self.lb.finish()
+
+    def set_loading_bar_goal(self, goal):
+        if self.lb is not None:
+            self.lb.set_goal(goal)
+
+    def set_loading_bar_status(self, status):
+        if self.lb is not None:
+            self.lb.set_status(status)
 
     def get_cursor(self):
         return self.db.connection.cursor()
@@ -17,13 +40,16 @@ class Migration:
 
     def migrate(self):
         cursor = self.get_cursor()
+        self.set_loading_bar_status('Running queries')
         for query in self.queries:
+            self.update_loading_bar(1)
             try:
                 cursor.execute(query)
             except Exception as e:
                 self.logger.error('Query failed: ' + query + ' with error: ' + str(e))
         self.db.connection.commit()
         cursor.close()
+        self.finish_loading_bar()
         self.reset_queries()
 
     def reset_queries(self):
--- migrations/database/M003Appliance.py
+++ migrations/database/M003Appliance.py
@@ -18,7 +18,10 @@ class M003Appliance(Migration):
 
     def insert(self, csv_path):
         data = pd.read_csv(csv_path, nrows=1).columns
+        self.set_loading_bar_goal(len(data) * 2)
+        self.set_loading_bar_status('Retrieving data')
         for name in data:
+            self.update_loading_bar(1)
             if name in self.exclude_columns():
                 continue
             self.add_sql(Query.INSERT_APPLIANCE.format(name))
