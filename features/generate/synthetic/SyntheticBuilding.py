966b49dcf98a5dad673b627d18186673ca085d5d
Reinder Noordmans <reindernoordmans@icloud.com>
2024-05-17 13:21:07 +0200

    feat(synthetic): Added classes

--- features/generate/synthetic/SyntheticBuilding.py
+++ features/generate/synthetic/SyntheticBuilding.py
@@ -2,6 +2,7 @@ from core.Database import Database
 from model.Building import Building
 from SQL.SQLQueries import BuildingOperations as Query
 
+
 class SyntheticBuilding:
     def __init__(self):
         id = Database.get_next_id(Building.table_name())
@@ -9,4 +10,3 @@ class SyntheticBuilding:
 
     def save(self):
         Database.query(Query.INSERT_BUILDING.format(self.name))
-
--- features/generate/synthetic/SyntheticPowerUsage.py
+++ features/generate/synthetic/SyntheticPowerUsage.py
@@ -1,4 +1,13 @@
+from core.Database import Database
+from SQL.SQLQueries import PowerUsageOperations as Query
 
 
 class SyntheticPowerUsage:
-    def __init__(self):
+    def __init__(self, building_id, datetime, power_usage):
+        self.building_id = building_id
+        self.datetime = datetime
+        self.power_usage = power_usage
+
+    def save(self):
+        Database.query(Query.INSERT_POWER_USAGE.format(self.building_id, self.datetime, self.power_usage))
+
